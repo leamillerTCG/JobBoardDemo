34c557a0d8c6acb517fbb1a951738c5abce1eb92
swetha-tcg <swetha.podduturi@careers.lon.ac.uk>
2015-03-11 09:56:58 +0000

    grid

---
+++ JobBoardTest/WebApplication1/Models/JobListModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class JobListModel
+    {
+        public List<Job> Jobs { get; set; }
+    }
+
+    public class Job
+    {
+        public int JobId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
