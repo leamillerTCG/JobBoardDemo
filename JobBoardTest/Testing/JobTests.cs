9f0627769ffb926aee392d148bdcd47a8bd2602e
Andy <andysum@yahoo.com>
2015-03-11 10:15:39 +0000

    Unit tests for create

--- JobBoardTest/Testing/JobTests.cs
+++ JobBoardTest/Testing/JobTests.cs
@@ -23,5 +23,18 @@ namespace Testing
 
             Assert.IsNotNull(jobs);
         }
+
+        [TestMethod]
+        public void CreateJobSuccess()
+        {
+            jobboardEntities dbContext = new jobboardEntities();
+
+            JobRepository jobRepository = new JobRepository(dbContext);
+
+            string title = "Test Title";
+            string description = "Test Description";
+
+            Assert.IsTrue(jobRepository.CreateJob(title, description));
+        }
     }
 }
